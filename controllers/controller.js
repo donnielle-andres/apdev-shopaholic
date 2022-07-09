2a72381f3eab6156c2bd064ffdf6f174697305b8
donnielle-andres <108661779+donnielle-andres@users.noreply.github.com>
2022-07-09 12:09:09 +0800

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,6 +10,7 @@ const router  = express.Router();
 const Product = require('../model/Product');
 const Review = require('../model/Review');
 const User = require('../model/User');
+// const { secret } = require('../config');
 
 
 const controller = {
